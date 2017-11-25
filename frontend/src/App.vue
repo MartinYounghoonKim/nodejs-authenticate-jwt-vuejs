da34b853c57cfac259df15b86740bb335b2ed1f6
martinyounghoonkim <hoons0131@gmail.com>
2017-11-25 20:38:28 +0900

    Create signin component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,23 +1,29 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <router-view/>
+    <div id="contents">
+      <router-view/>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'app'
-}
+};
 </script>
 
 <style>
 #app {
+  width: 600px;
+  margin: 60px auto 0;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+#contents {
+  text-align: left;
 }
 </style>
---
+++ frontend/src/components/Signin.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <form>
+      <b-form-group
+        label="Enter your user id"
+      >
+        <b-form-input type="text"></b-form-input>
+      </b-form-group>
+      <b-form-group
+        label="Enter your password"
+      >
+        <b-form-input type="password"></b-form-input>
+      </b-form-group>
+      <b-button size="lg" variant="success">Signin</b-button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Signin',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.btn-lg {
+  width: 100%;
+}
+</style>
