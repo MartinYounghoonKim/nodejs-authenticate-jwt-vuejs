30585d0b20b67a351b38c6a3856d299736401fd4
martinyounghoonkim <hoons0131@gmail.com>
2017-11-25 20:15:42 +0900

    Fix eslintrc rules

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -19,9 +19,12 @@ module.exports = {
   'rules': {
     // allow paren-less arrow functions
     'arrow-parens': 0,
+    'indent': [ 'error', 2 ],
     // allow async-await
     'generator-star-spacing': 0,
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'semi': [ 'error', 'always' ],
+    'no-extra-semi': 'error'
   }
 }
