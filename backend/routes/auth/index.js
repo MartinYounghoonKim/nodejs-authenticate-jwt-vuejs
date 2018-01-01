e121ae1b4bb4a422d9ae9ee0bcb0a49c426f15b9
martinyounghoonkim <hoons0131@gmail.com>
2018-01-01 14:39:16 +0900

    Update auth api routes comment

--- backend/routes/auth/index.js
+++ backend/routes/auth/index.js
@@ -1,8 +1,11 @@
+/**
+ * @desc auth router
+ */
+
 const express = require('express');
 const auth = express.Router();
 const controller = require('./controller');
 
-/* GET home page. */
 auth.get('/', controller.tester);
 
 module.exports = auth;
