16127968e47614097927d00cf6a378382e33df21
martinyounghoonkim <hoons0131@gmail.com>
2018-07-17 00:46:16 +0900

    Refactoring backend code by deleting useless codes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,9 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const auth = require('./auth/index');
-const board = require('./board/index');
 
 router.use('/', auth);
-router.use('/board', board);
 
 module.exports = router;
