a64741269d2a5d255c39b1de3e386097369ec430
martinyounghoonkim <hoons0131@gmail.com>
2018-07-17 00:48:19 +0900

    Fix typing error

--- backend/routes/auth/controller.js
+++ backend/routes/auth/controller.js
@@ -3,5 +3,9 @@ const authModels = require('../../models/auth/index');
 exports.getUsers = (req, res, next) => {
     authModels.getUsers(function(error, results) {
         res.json(results);
-    });
-}
+    }
+)};
+
+export.setUsers = (req, res, next) => {
+    console.log(req);
+};
