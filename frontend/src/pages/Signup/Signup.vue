b51f949680ddba4a9611ca337217a80028655dab
martinyounghoonkim <hoons0131@gmail.com>
2018-07-17 00:27:48 +0900

    Add signup axios logic

--- frontend/src/pages/Signup/Signup.vue
+++ frontend/src/pages/Signup/Signup.vue
@@ -1,55 +1,73 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <form>
-      <b-form-group
-        label="Enter your user id"
-      >
-        <b-form-input type="text"></b-form-input>
-      </b-form-group>
-      <b-form-group
-        label="Enter your password"
-      >
-        <b-form-input type="password"></b-form-input>
-      </b-form-group>
-      <b-form-group
-        label="Enter your password again"
-      >
-        <b-form-input type="password"></b-form-input>
-      </b-form-group>
-      <b-button size="lg" variant="success">Signup</b-button>
-    </form>
-  </div>
+    <div class="hello">
+        <h1>Signup Form</h1>
+        <form @submit.prevent="signup">
+            <b-form-group label="Enter your user id">
+                <b-form-input type="text"
+                              v-model="uid"></b-form-input>
+            </b-form-group>
+            <b-form-group label="Enter your password">
+                <b-form-input type="password"
+                              v-model="password"></b-form-input>
+            </b-form-group>
+            <b-button size="lg"
+                      variant="success"
+                      type="submit">
+                Signup
+            </b-button>
+        </form>
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'Signup',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+    import axios from 'axios';
+
+    export default {
+        name: 'Signup',
+        data () {
+            return {
+                uid: '',
+                password: ''
+            };
+        },
+        methods: {
+            signup () {
+                const uid = this.uid;
+                const password = this.password;
+                if (!uid || !password) {
+                    return false;
+                }
+
+                axios.post('http://localhost:3000/signup', { uid, password })
+                    .then(res => {
+                        console.log(res);
+                    });
+            }
+        }
     };
-  }
-};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-.btn-lg {
-  width: 100%;
-}
+    h1, h2 {
+        font-weight: normal;
+    }
+
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
+
+    a {
+        color: #42b983;
+    }
+
+    .btn-lg {
+        width: 100%;
+    }
 </style>
