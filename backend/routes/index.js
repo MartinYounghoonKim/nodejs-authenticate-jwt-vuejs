326b8c4117ff4f14b1a058f7b618f245e7c435f7
martinyounghoonkim <hoons0131@gmail.com>
2018-07-20 00:08:56 +0900

    Update auth's route uri

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
-
 const auth = require('./auth/index');
 
-router.use('/', auth);
+router.use('/auth', auth);
 
 module.exports = router;
