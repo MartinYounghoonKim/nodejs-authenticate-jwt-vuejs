ffe2571adc2165bb125f9ba5eb7ef6441d762dc0
martinyounghoonkim <hoons0131@gmail.com>
2018-07-19 23:24:47 +0900

    Add cors module in application's backend server

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const path = require('path');
-
+const cors = require('cors');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 
 const app = express();
 
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(cors());
 app.use(logger('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 /**
