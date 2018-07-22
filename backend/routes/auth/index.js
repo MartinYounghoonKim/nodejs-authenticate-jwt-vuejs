8f2d93500def3866f418d8dd09633c10712105c1
martinyounghoonkim <hoons0131@gmail.com>
2018-07-23 00:50:13 +0900

    Edit signin route

--- backend/routes/auth/index.js
+++ backend/routes/auth/index.js
@@ -7,7 +7,7 @@ const auth = express.Router();
 const controller = require('./controller');
 
 auth.get('/', controller.getUsers);
-auth.post('/', controller.signin);
+auth.post('/signin', controller.signin);
 auth.post('/signup', controller.signup);
 
 module.exports = auth;
