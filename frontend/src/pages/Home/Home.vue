63d2b44a0244ec90273be43280c0869c6600919e
martin <martin@within.co.kr>
2018-08-09 09:26:58 +0900

    Add Home page

---
+++ frontend/src/pages/Home/Home.vue
@@ -0,0 +1,62 @@
+<template>
+    <div>
+        <table class="table b-table table-striped table-hover">
+            <thead>
+                <tr>
+                    <th>번호</th>
+                    <th>제목</th>
+                    <th>작성일</th>
+                    <th>작성자</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="item in items">
+                    <td>{{ item.index }}</td>
+                    <td>{{ item.title }}</td>
+                    <td>{{ item.regDate }}</td>
+                    <td>{{ item.writer }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Home',
+        data () {
+            return {
+                items: [
+                    {
+                        index: 1,
+                        title: '게시글 제목',
+                        regDate: '2017-01-31',
+                        writer: '홍길동',
+                    },
+                    {
+                        index: 2,
+                        title: '게시글 제목',
+                        regDate: '2017-01-31',
+                        writer: '홍길동',
+                    },
+                    {
+                        index: 3,
+                        title: '게시글 제목',
+                        regDate: '2017-01-31',
+                        writer: '홍길동',
+                    },
+                    {
+                        index: 4,
+                        title: '게시글 제목',
+                        regDate: '2017-01-31',
+                        writer: '홍길동',
+                    }
+                ]
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,13 +1,19 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Signin from '@/pages/Signin/Signin.vue';
-import Signup from '@/pages/Signup/Signup';
+import Signup from '@/pages/Signup/Signup.vue';
+import Home from '@/pages/Home/Home.vue';
 
 Vue.use(Router);
 
 export default new Router({
     mode: 'history',
     routes: [
+        {
+            path: '/home',
+            name: 'Home',
+            component: Home
+        },
         {
             path: '/',
             name: 'Signin',
