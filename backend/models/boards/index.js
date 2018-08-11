f6067d41da714b190076f715758748f75bb68028
martinyounghoonkim <hoons0131@gmail.com>
2018-08-11 23:00:57 +0900

    Add get board content logic

--- backend/models/boards/index.js
+++ backend/models/boards/index.js
@@ -1,6 +1,37 @@
 const database = require('../index');
 const connection = database.connection();
 
+exports.getBoard = (index) => {
+    const sql = 'SELECT * FROM boards WHERE `index`=?';
+    return new Promise((resolve, reject) => {
+        connection.query(sql, [index], (err, result, fields) => {
+            if (err) {
+                reject({
+                    data: {},
+                    message: 'Something wrong in server.',
+                    status: 501,
+                });
+            } else {
+                const isExistResult = result.length > 0;
+                if (isExistResult) {
+                    resolve({
+                        status: 200,
+                        message: 'Success',
+                        data: result
+                    });
+                } else {
+                    reject({
+                        status: 401,
+                        message: 'Result is not existed.',
+                        data: {}
+                    });
+                }
+
+            }
+        })
+    });
+};
+
 exports.getBoards = () => {
     const sql = 'SELECT * FROM boards';
     return new Promise((resolve, reject) => {
--- backend/routes/boards/controller.js
+++ backend/routes/boards/controller.js
@@ -1,11 +1,24 @@
 const boardModels = require('../../models/boards/index');
 
 exports.getBoard = (req, res, next) => {
-    /**
-     * @TODO
-     * get detail board content
-     */
-
+    const { index } = req.params;
+    const indexToNumber = Number(index);
+    if (isNaN(indexToNumber)) {
+        res.status(401)
+            .json({
+                data: {},
+                message: 'This is not a valid request.'
+            });
+    }
+    boardModels.getBoard(index)
+        .then(result => {
+            res.json(result);
+        })
+        .catch(err => {
+            const { status } = err;
+            res.status(status)
+                .json(err);
+        });
 };
 
 exports.getBoards = (req, res, next) => {
@@ -21,6 +34,7 @@ exports.getBoards = (req, res, next) => {
 };
 
 exports.createBoard = (req, res, next) => {
+    const { uid, password } = req.body;
     boardModels.createBoard()
         .then(result => {
             res.json(result);
