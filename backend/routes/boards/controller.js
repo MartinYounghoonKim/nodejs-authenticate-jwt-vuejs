5c4c2d0bb5d1ecca2ff2fb2361f911cf0913f354
martinyounghoonkim <hoons0131@gmail.com>
2018-08-12 22:50:35 +0900

    Refactoring request's validate when user try to create board item

--- backend/routes/boards/controller.js
+++ backend/routes/boards/controller.js
@@ -35,12 +35,21 @@ exports.getBoards = (req, res, next) => {
 
 exports.createBoard = (req, res, next) => {
     const { uid, upk, title, content } = req.body;
-    const isInvalidRequest = !content || content.length <= 0 || !title || title.length <= 0;
+    const isExistTitle = title && title.length > 0;
+    const isExistContent = content && content.length > 0;
 
-    if (isInvalidRequest) {
+    if (!isExistTitle) {
         res.status(401)
             .json({
-                message: 'This is not a valid request.'
+                status: 401,
+                message: 'Title field is required.'
+            });
+    }
+    if (!isExistContent) {
+        res.status(401)
+            .json({
+                status: 401,
+                message: 'Content field is required.'
             });
     }
     boardModels.createBoard({ uid, upk, title, content })
