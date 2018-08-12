0d5825dc37e174ec43de2f8233962bf326d60735
martinyounghoonkim <hoons0131@gmail.com>
2018-08-12 23:20:24 +0900

    Add sample permission's middleware

--- backend/routes/boards/index.js
+++ backend/routes/boards/index.js
@@ -6,8 +6,10 @@ const express = require('express');
 const router = express.Router();
 const controller = require('./controller');
 const authMiddleware = require('../../middleware/auth');
+const permissionMiddleware = require('../../middleware/permission');
 
 router.use(authMiddleware);
+// router.use(['/', '/:index'], permissionMiddleware);
 
 router.get('/:index', controller.getBoard);
 router.get('/', controller.getBoards);
