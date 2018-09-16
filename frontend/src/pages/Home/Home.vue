587199e96c8bbd6884f6ea5bd2c8b4744fb99d87
martin <martin@within.co.kr>
2018-09-17 00:05:15 +0900

    Add required library and apply it in Home component

--- frontend/src/pages/Home/Home.vue
+++ frontend/src/pages/Home/Home.vue
@@ -13,8 +13,8 @@
                 <tr v-for="item in items">
                     <td>{{ item.index }}</td>
                     <td>{{ item.title }}</td>
-                    <td>{{ item.regDate }}</td>
-                    <td>{{ item.writer }}</td>
+                    <td>{{ item.regdate }}</td>
+                    <td>{{ item.user }}</td>
                 </tr>
             </tbody>
         </table>
@@ -22,8 +22,45 @@
 </template>
 
 <script>
+    import axios from 'axios';
+    import cookieJs from 'js-cookie';
+
+    function fetchBoardItems () {
+        return new Promise((resolve, reject) => {
+            axios.get('http://localhost:3000/board')
+                .then(res => {
+                    resolve(res.data.data);
+                })
+                .catch(reissueAccessToken)
+        })
+    }
+    function reissueAccessToken (err) {
+        return new Promise(resolve => {
+            const previos = err.response.config.url;
+
+            if (err.response.status === 401) {
+                axios.get('http://localhost:3000/auth/reissuance')
+                    .then(res => {
+                        cookieJs.set('accessToken', res.data.data.accessToken);
+                        axios.defaults.headers.common['x-access-token'] = cookieJs.get('accessToken');
+                        axios.get(previos)
+                            .then(res => {
+                                console.log(res);
+                            });
+                    });
+            }
+        })
+    }
     export default {
         name: 'Home',
+        created () {
+            axios.defaults.headers.common['x-access-token'] = cookieJs.get('accessToken');
+            axios.defaults.headers.common['x-refresh-token'] = cookieJs.get('refreshToken');
+            fetchBoardItems()
+                .then(items => {
+                    this.items = items;
+                })
+        },
         data () {
             return {
                 items: [
@@ -53,6 +90,22 @@
                     }
                 ]
             }
+        },
+        methods: {
+            reissueToken (err) {
+                const previos = err.response.config.url;
+                if (err.response.status === 401) {
+                    axios.get('http://localhost:3000/auth/reissuance')
+                        .then(res => {
+                            cookieJs.set('accessToken', res.data.data.accessToken);
+                            axios.defaults.headers.common['x-access-token'] = cookieJs.get('accessToken');
+                            axios.get('http://localhost:3000/board')
+                                .then(res => {
+                                    console.log(res);
+                                });
+                        });
+                }
+            },
         }
     }
 </script>
